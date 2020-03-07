365169cf2c95a92153a9c2acdf2f0b7b9a0d53dc
supriyaphalle <supriyaphalle@gmail.com>
2020-03-07 11:57:41 +0530

    ADDED:welcome message

---
+++ src/main/java/iplanalyser/IPLAnalyser.java
@@ -0,0 +1,11 @@
+package iplanalyser;
+
+import CSVReader.ICSVBuilder;
+import CSVReader.OpenCSVBuilder;
+
+public class IPLAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Ipl- 2K19 Analyser");
+    }
+
+}
---
+++ src/test/java/iplanalyser/IPLAnalyserTest.java
@@ -0,0 +1,13 @@
+package iplanalyser;
+
+import org.junit.Test;
+
+public class IPLAnalyserTest {
+
+
+
+    @Test
+    public void givenIPLRunsData_whenSortedOnBattingAvg_ShouldReturnTrue() {
+        IPLAnalyser iplAnalyser = new IPLAnalyser();
+    }
+}
